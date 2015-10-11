0fb6188bc350fe2620c4913212cfe8d05cc2fbb9
Martin Kask <kask.martin@gmail.com>
2015-10-11 16:09:14 +0300

    Laevade pommitamine tehtud - 2D versioon

--- src/teema1/Peamurdja3_laevad.java
+++ src/teema1/Peamurdja3_laevad.java
@@ -16,28 +16,39 @@ import java.util.Scanner;
  */
 public class Peamurdja3_laevad {
     public static void main(String[] args)   {
-        int suurus = 0;
-        while (suurus < 2 || suurus > 50) {
-            System.out.println("Kui suur m2nguv2li peaks olema? (2-50)");
+        int row = 0;
+        int col = 0;
+        while (row < 1 || row > 10) {
+            System.out.println("Kui pikk m2nguv2li peaks olema? (1-10)");
             Scanner sc = new Scanner(System.in);
-            suurus = sc.nextInt();
+            row = sc.nextInt();
         }
-        char[] manguLaud = new char[suurus];
-        char[] visualLaud = new char[suurus];
 
-        for (int i = 0; i < visualLaud.length; i++) {
-            visualLaud[i] = '#';
+        while (col < 1 || col > 10) {
+            System.out.println("Kui lai m2nguv2li peaks olema? (1-10)");
+            Scanner sc = new Scanner(System.in);
+            col = sc.nextInt();
+        }
+        char[][] manguLaud = new char[row][col];
+        char[][] visualLaud = new char[row][col];
+
+        for (int k = 0; k < visualLaud.length; k++) {
+            for (int i = 0; i < visualLaud[k].length; i++) {
+                visualLaud[k][i] = '#';
+            }
+        }
+
+        for (int k = 0; k < manguLaud.length; k++) {
+            for (int i = 0; i < manguLaud[k].length; i++) {
+                manguLaud[k][i] = '#';
+            }
         }
 
         int laevadeArv = -1;
-        while (laevadeArv < 0 || laevadeArv > suurus) {
-            System.out.format("Kui palju laevu peaks olema? (0-%d)%n", suurus);
+        while (laevadeArv < 0 || laevadeArv > row * col) {
+            System.out.format("Kui palju laevu peaks olema? (0-%d)%n", row * col);
             Scanner sc = new Scanner(System.in);
             laevadeArv = sc.nextInt();
-        }
-
-        for (int i = 0; i < manguLaud.length; i++) {
-            manguLaud[i] = '#';
         }
 
         int shipsCount = laevadeArv;
@@ -45,39 +56,55 @@ public class Peamurdja3_laevad {
 
             while (laevadeArv > 0) {
             int y = (int) Math.floor(Math.random() * manguLaud.length);
-            if (manguLaud[y] == '#') {
-                manguLaud[y] = 'V';
+            int x = (int) Math.floor(Math.random() * manguLaud[0].length);
+            if (manguLaud[y][x] == '#') {
+                manguLaud[y][x] = 'V';
                 laevadeArv--;
             }
         }
 
-        int positsioon = -1;
+        int positsioonX = -1;
+        int positsioonY = -1;
         boolean gameover = false;
         int pommitusi = 0;
 
 
         while (!gameover) {
-            System.out.println(visualLaud);
-            positsioon = -1;
-            while (positsioon < 0 || suurus > visualLaud.length) {
-                System.out.format("Ytle positsioon mida pommitada (0-%d)%n", visualLaud.length);
-                Scanner sc = new Scanner(System.in);
-                positsioon = sc.nextInt();
+
+            for (int k = 0; k < visualLaud.length; k++) {
+                for (int i = 0; i < visualLaud[k].length; i++) {
+                    System.out.print(visualLaud[k][i]);
+                }
+                System.out.println();
+
             }
 
-            if (manguLaud [positsioon] == 'V'){
+            positsioonX = -1;
+            positsioonY = -1;
+            while (positsioonX < 0 || positsioonX > visualLaud[0].length-1) {
+                System.out.format("Ytle mitmendat postisiooni reas pommitada (0-%d)%n", visualLaud[0].length-1);
+                Scanner sc = new Scanner(System.in);
+                positsioonX = sc.nextInt();
+            }
+            while (positsioonY < 0 || positsioonY > visualLaud[0].length-1) {
+                System.out.format("Ytle mitmendat rida pommitada (0-%d)%n", visualLaud.length-1);
+                Scanner sc = new Scanner(System.in);
+                positsioonY = sc.nextInt();
+            }
+
+            if (manguLaud [positsioonY][positsioonX] == 'V'){
                 System.out.println("Pihtas");
-                manguLaud [positsioon] = 'x';
-                visualLaud [positsioon] = 'x';
+                manguLaud [positsioonY][positsioonX] = 'x';
+                visualLaud [positsioonY][positsioonX] = 'x';
                 shipsCount--;
             }
-            else if (manguLaud [positsioon] == 'x' || manguLaud [positsioon] == '0'){
+            else if (manguLaud [positsioonY][positsioonX] == 'x' || manguLaud [positsioonY][positsioonX] == '0'){
                 System.out.println("Siia oled juba pommitanud");
             }
             else {
                 System.out.println("M66das");
-                manguLaud [positsioon] = '0';
-                visualLaud [positsioon] = '0';
+                manguLaud [positsioonY][positsioonX] = '0';
+                visualLaud [positsioonY][positsioonX] = '0';
             }
             pommitusi++;
 
